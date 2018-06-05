d514f47cd6bf2ef1830d9397dba988a3b7b6133c
jerrypan90 <jerrypan_90@hotmail.com>
2018-06-05 17:55:37 +0800

    changed webpage title to test

--- src/component/logo.js
+++ src/component/logo.js
@@ -7,7 +7,7 @@ class Logo extends Component {
     return (
       <div id="logoDiv">
         <img id='logo' src='http://dribbble.s3.amazonaws.com/users/272547/screenshots/915839/rp.jpg' alt='PlayRoam Logo' />
-        <h1 id='logoName'>PlayRoam</h1>
+        <h1 id='logoName'>Testing</h1>
       </div>
     )
   }
